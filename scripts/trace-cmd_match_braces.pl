59f73d8b4544a0f1991f5e8655ffbb2d82281614
Frank Sorenson <sorenson@redhat.com>
2019-12-23 15:38:03 -0600

    trace-cmd_match-braces: handle multiple pids
    The original version would only handle a single pid
    correctly.  Use a stack for each pid, rather than a
    single, common stack.
    
    Signed-off-by: Frank Sorenson <sorenson@redhat.com>

--- scripts/trace-cmd_match_braces.pl
+++ scripts/trace-cmd_match_braces.pl
@@ -3,32 +3,99 @@
 use strict;
 use warnings;
 
-my @stack = ();
+my %stacks = ();
+
+my $pid_width = 25;
 
 while(<>) {
 	chomp;
 	my $line = $_;
 
-	if ($line =~ /(.+) \|([ ]+)([0-9a-zA-Z_\.]+)( \[[a-zA-Z0-9_\.]+\])?\(\) \{/) {
-		my $func = $3;
+
+#	if ($line =~ /^ (.*<\.\.\.>-[0-9]+)(.+)/) { # these are misaligned, for some reason
+#		$line = sprintf("%s%s", $1, $2);
+#	}
+
+	if ($line =~ /^([ ]*)(.+) (\[[0-9]+\]) (.+): (funcgraph_entry:) (.+)\|([ ]+)([0-9a-zA-Z_\.]+)( \[[a-zA-Z0-9_\.]+\])?\(\) \{/) {
+#          fsperf-2257  [087] 443018.640870: funcgraph_entry:                   |  call_transmit() {
+#          fsperf-2257  [084] 443018.617221: funcgraph_entry:        0.674 us   |  xprt_release_xprt();
+#          fsperf-2257  [084] 443018.617228: funcgraph_entry:        0.053 us   |  xprt_reserve_xprt();
+#          fsperf-2257  [084] 443018.617245: funcgraph_entry:        0.167 us   |  xprt_reserve_xprt();
+#           <...>-172660 [084] 443018.617283: funcgraph_entry:        1.468 us   |  xprt_release_xprt();
+
+		my $proc_pid = $2;
+		my $cpu = $3;
+		my $start_time = $4;
+		my $funcgraph_entry = $5;
+		my $stuff = $6;
+
+		my $spaces = $7;
+
+		my $func = $8;
 		my $module = "";
-		if (defined $4) {
-			$module = $4;
+		if (defined $9) {
+			$module = $7;
 		}
-		my $result = "$func$module";
-		printf("%s |%s%s() {\n", $1, $2, $result);
-#		printf("function start '%s' in '%s' (indent = %d)\n", $result, $line, length($2));
-		push @stack, $result;
-	} elsif ($line =~ /(.+) \|([ ]+)}$/) {
-#		printf("function complete '%s' in '%s' (indent = %d)\n", $1, $line, length($2));
-		my $fn;
-		if ($#stack > -1) {
-			$fn = pop @stack;
+		my $func_mod = "$func$module";
+
+		my $proc_name = "";
+		my $pid = "";
+
+		if ($proc_pid =~ /(.+)-([0-9]+)$/) {
+			$proc_name = $1;
+			$pid = $2;
 		} else {
-			$fn = "???";
+			$proc_name = "???";
+			$pid = $proc_pid;
 		}
+
+		my %new_ele = ( 'func_mod' => $func_mod, 'start_time' => $start_time );
+
+		printf("%*s %s %s: %s %s|%s%s() {\n", $pid_width, $proc_pid, $cpu, $start_time, $funcgraph_entry, $stuff, $spaces, $func_mod);
+
+#		push @{$stacks{$pid}}, { 'func_mod' => $func_mod, 'start_time' => $start_time };
+		push @{$stacks{$pid}}, \%new_ele;
+	} elsif ($line =~ /([ ]*)(.+) (\[[0-9]+\]) (.+): (funcgraph_exit:) (.+)\|([ ]+)}$/) {
+#		<...>-183870 [090] 443031.690628: funcgraph_exit:         4.788 us   |    }
+		my $start_time = "";
+
+		my $proc_pid = $2;
+		my $proc_name = "";
+		my $pid = "$proc_pid";
+		my $cpu = $3;
+		my $curr_time = $4;
+		my $funcgraph_exit = $5;
+		my $stuff = $6;
+		my $spaces = $7;
+
+		my $fn;
+
+		if ($proc_pid =~ /(.+)-([0-9]+)$/) {
+			$proc_name = $1;
+			$pid = $2;
+		}
+
+		if (!defined($stacks{$pid})) {
+			$stacks{$pid} = [];
+		}
+		#		printf("scalar \@{\$stacks{%d}} = %d\n", $pid, scalar @{$stacks{$pid}});
+
+		if (scalar @{$stacks{$pid}} > 0) {
+			my %fn_time;
+			%fn_time = %{ pop @{$stacks{$pid}} };
+			$fn = $fn_time{'func_mod'};
+			$start_time = $fn_time{'start_time'};
+		} else {
+			$fn = "????";
+		}
+
+
 #		printf("%s complete\n", $fn);
-		printf("%s |%s} /* %s */\n", $1, $2, $fn);
+#		<...>-183870 [090] 443031.690628: funcgraph_exit:         4.788 us   |    }
+		printf("%*s %s %s: %s %s|%s} /* %s */\n", $pid_width, $proc_pid, $cpu, $curr_time, $funcgraph_exit, $stuff, $spaces, $fn);
+	} elsif ($line =~ /([ ]*)(.+-[0-9]+) (.+)$/) {
+#          <...>-43912 [226] 276690.582264: funcgraph_entry:        0.198 us   |  xprt_reserve_xprt();
+		printf("%*s %s\n", $pid_width, $2, $3);
 	} else {
 		printf("%s\n", $line);
 	}
