8cc17723b72b3bf32b5fb941ebe0f9397e4060fd
Frank Sorenson <sorenson@redhat.com>
2021-10-21 20:20:42 -0500

    get_handle: Major rewrite
    Major rewrite of explanations of the error results and
    display of the effective path on which the
    name_to_handle_at() will operate.
    
    Signed-off-by: Frank Sorenson <sorenson@redhat.com>

--- test_file_handles/get_handle.c
+++ test_file_handles/get_handle.c
@@ -3,10 +3,9 @@
 
 	Test the name_to_handle_at() syscall
 
-	Usage:  get_handle <path> [ <subdir> ]
+	Usage:  get_handle [ <path> [ <subpath> ] ]
 
 	TODO: also test open_by_handle_at() syscall and related library calls
-
 */
 
 #define _GNU_SOURCE
@@ -37,16 +36,34 @@
 #define path_relative(p)	(p && p[0] != '\0' && p[0] != '/')
 #define path_absolute(p)	(p && p[0] == '/')
 
-void do_try_name_to_handle_at(const char *path, const char *subdir, int flags) {
-	struct file_handle *fhp = malloc(4096);
+int call_name_to_handle_at(const char *path, const char *subpath) {
+	char *display_path = NULL;
+	char *cwd = NULL;
+
+	struct file_handle *fhp = malloc(BUF_SIZE);
 	unsigned int fhsize = MAX_HANDLE_SZ;
-	int mount_id, dirfd, i;
+	int dfd = AT_FDCWD, ret = EXIT_FAILURE, flags = 0, mount_id, i;
 	char *flags_str;
 
-	if ((dirfd = openat(AT_FDCWD, path, O_RDONLY|O_CLOEXEC|O_PATH)) < 0) {
-		printf("error opening '%s': %m\n", path);
-		goto out;
+
+	cwd = get_current_dir_name();
+	if (path_have(path)) {
+		if (path_empty(path)) {
+			path = ".";
+		}
+		if ((dfd = openat(AT_FDCWD, path, O_RDONLY|O_CLOEXEC|O_PATH)) < 0) {
+			if (path_empty_or_dot(path))
+				output("error opening the current directory (\"%s\"): %m\n", cwd);
+			else
+				output("error opening \"%s\": %m\n", path);
+			goto out;
+		}
 	}
+	if (path_null_or_empty(subpath)) {
+		flags |= AT_EMPTY_PATH;
+		subpath = "";
+	}
+	fhp->handle_bytes = fhsize;
 
 	switch (flags) {
 		case 0: flags_str = "0"; break;
@@ -55,66 +72,107 @@ void do_try_name_to_handle_at(const char *path, const char *subdir, int flags) {
 		case AT_EMPTY_PATH | AT_SYMLINK_FOLLOW: flags_str = "AT_EMPTY_PATH | AT_SYMLINK_FOLLOW"; break;
 		default: asprintf(&flags_str, "INVALID: 0x%x", flags); break;
 	}
-retry_name_to_handle_at:
-	fhp->handle_bytes = fhsize;
-	if (fhp->handle_bytes > (BUF_SIZE - offsetof(struct file_handle, f_handle))) {
-		printf("error...  fhsize of %u is too large\n", fhp->handle_bytes);
-		goto out;
-	}
 
-	printf("name_to_handle_at(%s/%s) with fhsize %u, flags: %s", path, subdir, fhsize, flags_str);
+	// display strings -- what path are we actually looking at?
+	if (path_absolute(subpath))
+	//	empty		absolute	subpath
+	//	absolute	absolute	subpath
+	//	relative	absolute	subpath
+		display_path = strdup(subpath);
+	else if (
+		((!path_have(path)) && (!path_have(subpath))) ||
+		(path_empty_or_dot(path) && path_null_or_empty_or_dot(subpath))
+		)
+	//	none		none		cwd
+	//	empty		none		cwd
+	//	empty		empty		cwd
+		display_path = strdup(cwd);
+	else if (path_absolute(path) && path_null_or_empty_or_dot(subpath))
+	//	absolute	none		path
+	//	absolute	empty		path
+		display_path = strdup(path);
+	else if (path_relative(path) && path_null_or_empty_or_dot(subpath))
+	//	relative	none		cwd/path
+	//	relative	empty		cwd/path
+		asprintf(&display_path, "%s/%s", cwd, path);
+	else if (path_absolute(path) && path_relative(subpath))
+	//	absolute	relative	path/subpath
+		asprintf(&display_path, "%s/%s", path, subpath);
+	else if (path_empty_or_dot(path) && path_relative(subpath))
+	//	empty		relative	cwd/subpath
+		asprintf(&display_path, "%s/%s", cwd, subpath);
+	else if (path_relative(path) && path_relative(subpath))
+	//	relative	relative	cwd/path/subpath
+		asprintf(&display_path, "%s/%s/%s", cwd, path, subpath);
+	else
+		display_path = strdup("INVALID");
+	//	none		empty		"INVALID"
+	//	none		absolute	"INVALID"
+	//	none		relative	-- INVALID
 
-	if ((name_to_handle_at(dirfd, subdir, fhp, &mount_id, flags)) < 0) {
-		if (errno == EOPNOTSUPP) {
-			printf(": not supported: %m\n");
-			goto out;
-		} else if (errno == EOVERFLOW) {
+	output("effective path: \"%s\"\n", display_path);
+	output("name_to_handle_at(%d<%s>, \"%s\", file_handle{handle_bytes = %d}, &mount_id, %s)",
+		dfd, path, subpath, fhp->handle_bytes, flags_str);
+
+	if ((name_to_handle_at(dfd, subpath, fhp, &mount_id, flags)) < 0) {
+		if (errno == EINVAL) {
+			output("EINVAL - this should not happen, unless MAX_HANDLE_SZ (%d)\n", MAX_HANDLE_SZ);
+			output("  is increased in userspace above the kernel value\n");
+		} else if (errno == EOPNOTSUPP)
+			output("EOPNOTSUPP - the underlying filesystem may not support filehandles\n");
+		else if (errno == ENOENT)
+			output("ENOENT - the path \"%s\" may not exist\n", display_path);
+		else if (errno == ENOTDIR)
+			output("ENOTDIR - a path component of \"%s\" may not be a directory\n", display_path);
+		else if (errno == EOVERFLOW) {
 			if (fhp->handle_bytes > MAX_HANDLE_SZ) {
-				printf(": EOVERFLOW - returned fhsize of %u - the filesystem may already have filehandles larger than representable within %d bytes (filehandles may be larger than %d bytes)\n",
-					fhp->handle_bytes, MAX_HANDLE_SZ, MAX_HANDLE_SZ - 16); /* as of kernel 5.14, 16 bytes */
-				goto out;
-			} else if (fhp->handle_bytes > fhsize) { /* probably won't happen, since the previous case will likely occur */
-				printf(": EOVERFLOW - returned fhsize of %u - extending fhsize to %u\n", fhp->handle_bytes, fhsize);
-				goto retry_name_to_handle_at;
+				output("EOVERFLOW - the filesystem exports filehandles of %d bytes,", fhp->handle_bytes);
+				output("  but the largest size supported is %d bytes\n", MAX_HANDLE_SZ);
 			} else {
-				printf(": EOVERFLOW - returned fhsize of %u, but called with %u bytes.\n",
-					fhp->handle_bytes, fhsize);
-				printf("    one of the following is likely true:\n");
-				printf("        * no file handle is available for this particular name on a filesystem\n");
-				printf("          which normally supports file handle lookup\n");
-				printf("        * the filesystem supports both file handles and automount points, and\n");
-				printf("          %s is an automount point\n",
-					(!strcmp(subdir, "")) ? "the requested name" : subdir);
-				goto out;
+				output("EOVERFLOW - one of the following is likely true:\n");
+				output("\t* no file handle is available for this particular name on a filesystem\n");
+				output("\t  which normally supports file handle lookup\n");
+				output("\t* the filesystem supports both file handles and automount points, and\n");
+				output("\t  %s is an automount point\n", display_path);
 			}
-		} else {
-			printf(": error: %m\n");
-			goto out;
-		}
+		} else
+			output("ERROR occurred - %m\n");
+
 	} else {
-		printf(": success\n");
-		printf("mount_id: %d\n", mount_id);
-		printf("filehandle size: %u  handle type: %d\n", fhp->handle_bytes, fhp->handle_type);
-		printf("handle: ");
+		output(": success\n");
+		output("mount_id: %d  filehandle size: %u  handle type: %d\n", mount_id, fhp->handle_bytes, fhp->handle_type);
+		output("handle: ");
 		for (i = 0 ; i < fhp->handle_bytes ; i++)
-			printf(" %02x", fhp->f_handle[i]);
-		printf("\n");
+			output(" %02x", fhp->f_handle[i]);
+		output("\n");
+		ret = EXIT_SUCCESS;
 	}
 out:
+	if (cwd)
+		free(cwd);
+	if (display_path)
+		free(display_path);
 	if ((flags & ~(AT_EMPTY_PATH | AT_SYMLINK_FOLLOW)) != 0)
 		free(flags_str);
+	return ret;
+}
+int usage(const char *exe, int ret) {
+	output("Usage: %s [ <path> [ <subpath> ] ]\n", exe);
+	output("\n");
+//	output("	no-argument usage
+//	output("	one-argument usage
+//	output("	two-argument usage
+
+	return ret;
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 2 && argc != 3) {
-		printf("Usage: %s <path> [ <subdir> ]\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	if (argc == 2)
-		do_try_name_to_handle_at(argv[1], "", AT_EMPTY_PATH);
+	if (argc == 1)
+		call_name_to_handle_at(NULL, NULL);
+	else if (argc == 2)
+		call_name_to_handle_at(argv[1], NULL);
 	else if (argc == 3)
-		do_try_name_to_handle_at(argv[1], argv[2], 0);
-
-	return EXIT_SUCCESS;
+		call_name_to_handle_at(argv[1], argv[2]);
+	else
+		return usage(argv[0], EXIT_FAILURE);
 }
