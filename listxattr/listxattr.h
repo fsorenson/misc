acd4e5488ee79d27bb3c777cf63491e3a8e23e65
Frank Sorenson <sorenson@redhat.com>
2023-07-31 15:48:35 -0500

    listxattr: belated commit of exit_fail, other minor code
    Very minor code
    
    Signed-off-by: Frank Sorenson <sorenson@redhat.com>

--- listxattr/listxattr.h
+++ listxattr/listxattr.h
@@ -12,6 +12,7 @@
 #define __LISTXATTR_H__
 
 #include "lib.h"
+#include <linux/xattr.h>
 
 #define ACL_NFS4_XATTR		"system.nfs4_acl"
 #define DACL_NFS4_XATTR		"system.nfs4_dacl"
@@ -29,6 +30,13 @@
 	printf(args); \
 	fflush(stdout); \
 } while (0)
+
+#define exit_fail(args...) do { \
+	output(args); \
+	exit(-1); \
+} while (0)
+
+typedef int (show_acl_t)(const char *attr_name, const unsigned char *attr_bytes, int len, bool is_dir);
 
 /* lots of xattrs defined in include/uapi/linux/xattr.h */
 
