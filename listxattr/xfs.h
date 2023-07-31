807663663c75acc5c9704732a052e46e687b39a7
Frank Sorenson <sorenson@redhat.com>
2023-07-31 13:20:02 -0500

    listxattr: convert the xfs encdec to the new format
    Convert xfs encdec to new format
    
    Signed-off-by: Frank Sorenson <sorenson@redhat.com>

--- listxattr/xfs.h
+++ listxattr/xfs.h
@@ -10,13 +10,10 @@
 #define __LISTXATTR_XFS_H__
 
 #include <xfs/xfs.h>
-//#include <xfs/xfs_fs.h>
-//#include <xfs/linux.h>
-//#include <xfs/xfs_types.h>
-//#include <xfs/xfs_fs_compat.h>
 #include <xfs/xfs_format.h>
 #include "listxattr.h"
+#include "encdec.h"
 
-void *show_xfs_acl(const char *name, const unsigned char *buf, int len, bool is_dir);
+int decode_xfs_acl(const char *name, const unsigned char *buf, int len, bool is_dir);
 
 #endif /* __LISTXATTR_XFS_H__ */
