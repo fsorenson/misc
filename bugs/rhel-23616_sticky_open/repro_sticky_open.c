49f226b0ae0b84808270e0c6e2703fc757c62b52
Frank Sorenson <sorenson@redhat.com>
2024-02-02 16:06:56 -0600

    bugs/rhel-23616_repro_sticky_open: Test O_CREAT and sysctl as well
    The issue occurs only when O_CREAT is specified, and only
    when the fs.protected_regular sysctl is set.
    
    Add testing for the various combinations.

--- bugs/rhel-23616_sticky_open/repro_sticky_open.c
+++ bugs/rhel-23616_sticky_open/repro_sticky_open.c
@@ -1,5 +1,14 @@
 /*
 	Frank Sorenson <sorenson@redhat.com>, 2024
+
+	setup - create a group and two user accounts:
+	# groupadd -g 2000 group2000
+	# useradd -g 2000 -u 2000 user2000
+	# useradd -g 2000 -u 2001 user2001
+
+	with sticky bit on the directory, 777 permissions, opening
+	an existing file with O_CREAT when not the owner fails
+
 */
 
 #include <stdio.h>
@@ -19,7 +28,26 @@
 #define FILENAME "testfile2000"
 #define FULLPATH DIRECTORY "/" FILENAME
 
+#define ARRAY_SIZE(a) ( sizeof(a)/sizeof(a[0]) )
+
 char *path = DIRECTORY;
+int dfd;
+
+int set_protected(int val) {
+	char buf = (char)(val + '0');
+	int fd, ret = 0;
+
+	if ((fd = open("/proc/sys/fs/protected_regular", O_WRONLY)) < 0) {
+		printf("error opening sysctl: %m\n");
+		return 1;
+	}
+	if ((ret = write(fd, &buf, 1)) != 1) {
+		printf("error writing %d to sysctl: %m\n", val);
+	}
+	close(fd);
+
+	return ret == 1 ? 0 : 1;
+}
 
 int test_openas(int dfd, uid_t uid, int flags) {
 	int fd, status;
@@ -27,23 +55,66 @@ int test_openas(int dfd, uid_t uid, int flags) {
 
 	if ((cpid = fork()) == 0) {
 		if (setgid(GID))
-			printf("error with setgid: %m\n");
+			printf("      error with setgid: %m\n");
 		if (setuid(uid))
-			printf("error with setuid: %m\n");
+			printf("      error with setuid: %m\n");
 
 		if ((fd = openat(dfd, FILENAME, flags, 0664)) < 0) {
-			printf("error opening '%s/%s': %m\n", path, FILENAME);
+			printf("      error opening '%s/%s': %m\n", path, FILENAME);
 			exit(EXIT_FAILURE);
 		}
 		close(fd);
 		exit(EXIT_SUCCESS);
 	}
 	waitpid(cpid, &status, 0);
-	return EXIT_SUCCESS;
+	return 0;
+}
+#define do_test(flags) do { \
+	printf("    testing open as uid %d with open flags %s\n", UID2, #flags); \
+	ret += test_openas(dfd, UID2, flags); \
+} while (0)
+
+int test_dir_mode(int mode) {
+	int ret = 0;
+
+	printf("  setting directory mode to %o\n", mode);
+	if ((fchmod(dfd, mode))) {
+		printf("error changing mode of '%s' to %o: %m\n", path, mode);
+		return 1;
+	}
+
+	do_test(O_WRONLY);
+	do_test(O_WRONLY|O_CREAT);
+
+	return ret;
 }
 
+int test_protected(int val) {
+	int modes[] = { 0777, 01777 }, i, ret = 0;
+
+	printf("setting sysctl fs.protected_regular to %d\n", val);
+	set_protected(val);
+
+	for (i = 0 ; i < ARRAY_SIZE(modes) ; i++) {
+		ret += test_dir_mode(modes[i]);
+		printf("\n");
+	}
+
+	return ret;
+}
+
+int do_testing(void) {
+	int i, ret = 0;
+
+	for (i = 0 ; i <= 2 ; i++)
+		ret += test_protected(i);
+
+	return ret;
+}
+
+
 int main(int argc, char *argv[]) {
-	int dfd, fd;
+	int fd, ret = 0;
 
 	if (argc > 1)
 		path = argv[1];
@@ -68,21 +139,7 @@ int main(int argc, char *argv[]) {
 	fchown(fd, UID1, GID);
 	close(fd);
 
-	printf("setting directory mode to 1777\n");
-	if ((fchmod(dfd, 01777)))
-		printf("error changing mode of '%s' to 01777: %m\n", path);
+	ret += do_testing();
 
-	printf("testing open as uid %d with open flags O_WRONLY|O_CREAT\n", UID2);
-	test_openas(dfd, UID2, O_WRONLY|O_CREAT);
-
-	printf("\n");
-
-	printf("setting directory mode to 0777\n");
-	if ((fchmod(dfd, 0777)))
-		printf("error changing mode of '%s' to 0777: %m\n", path);
-
-	printf("testing open as uid %d with open flags O_WRONLY|O_CREAT\n", UID2);
-	test_openas(dfd, UID2, O_WRONLY|O_CREAT);
-
-	return EXIT_SUCCESS;
+	return ret;
 }
