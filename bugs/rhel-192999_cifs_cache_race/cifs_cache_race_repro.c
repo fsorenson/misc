a680aa7d22875a0a67664953ecc40b301a62775f
Frank Sorenson <frank.sorenson@gmail.com>
2026-07-17 13:47:25 -0500

    bugs/rhel-192999 cifs_cache_race_repro.c
    Reproducer for a race that results in directory cache corruption
    when multiple threads are renaming files in a directory
    simultaneously with threads causing readdir.
    
    Signed-off-by: Frank Sorenson <frank.sorenson@gmail.com>

---
+++ bugs/rhel-192999_cifs_cache_race/cifs_cache_race_repro.c
@@ -0,0 +1,184 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <errno.h>
+
+#define NUM_THREADS 6
+
+#define free_str(x) do { \
+	if (x) \
+		free(x); \
+	x = NULL; \
+} while (0)
+#define close_fd(x) do { \
+	if (x >= 0) \
+		close(x); \
+	x = -1; \
+} while (0)
+
+int process_one(int threadnum, int filenum) {
+	int fd0 = -1, fd1 = -1, fd2 = -1, fd3 = -1, dfd0 = -1;
+	char *filename0 = NULL, *filename1 = NULL, *filename2 = NULL, *filename3 = NULL, *filename4 = NULL, *filename5 = NULL, buf[4096];
+	struct stat st1, st2;
+	int ret = EXIT_FAILURE;
+	int count;
+
+	asprintf(&filename0, "work/file_%d_%d.xml.stringreplace.zip", threadnum, filenum);
+	if ((fd0 = open(filename0, O_CREAT|O_TRUNC|O_RDWR, 0644)) < 0) { // "work/file_1_1.xml_stringreplace.zip"
+		printf("error opening %s: %m\n", filename0);
+		goto out;
+	}
+	asprintf(&filename1, "file_%d_%d.xml", threadnum, filenum);
+	if ((fd1 = open(filename1, O_RDONLY)) < 0) { // file_1_1.xml
+		printf("error opening %s: %m\n", filename1);
+		goto out;
+	}
+
+	count = read(fd1, buf, sizeof(buf));
+//	Data (1290 bytes)
+
+	write(fd0, buf, 816);
+//	Data (816 bytes)
+
+	asprintf(&filename2, "work/file_%d_%d.xml__temp", threadnum, filenum);
+	if ((fd2 = open(filename2, O_CREAT|O_TRUNC|O_RDWR, 0644)) < 0) { // "work/file_1_1.xml__temp"
+		printf("error opening %s: %m\n", filename2);
+		goto out;
+	}
+	write(fd2, buf, 1290);
+
+	asprintf(&filename3, "work/file_%d_%d.xml", threadnum, filenum);
+	if (stat(filename3, &st1) == 0 || errno != ENOENT) {
+		if (errno != ENOENT)
+			printf("file '%s' should not have existed: %m\n", filename3);
+		else
+			printf("file '%s' should not have existed\n", filename3);
+		goto out;
+	}
+				; // "work/file_1_1.xml"
+	if (stat(filename3, &st1) == 0 || errno != ENOENT) { // "work/file_1_1.xml"
+		if (errno != ENOENT)
+			printf("file '%s' should not have existed: %m\n", filename3);
+		else
+			printf("file '%s' should not have existed\n", filename3);
+		goto out;
+	}
+
+	close_fd(fd2);
+
+	stat(filename2, &st1);
+	if (rename(filename2, filename3) < 0) { // "work/file_1_1.xml__temp", "work/file_1_1.xml"
+		printf("error renaming %s -> %s: %m\n", filename2, filename3);
+		goto out;
+	}
+	stat(filename3, &st2);
+	if ((intmax_t)st1.st_size != (intmax_t)st2.st_size) {
+		printf("file size prior to rename: %ld, file size after rename: %ld\n", (intmax_t)st1.st_size, (intmax_t)st2.st_size);
+		goto out;
+	}
+	close_fd(fd1);
+
+	if (unlink(filename1) < 0) { // file_1_1.xml
+		printf("error removing %s: %m\n", filename1);
+		goto out;
+	}
+	close_fd(fd0);
+
+//	dfd0 = open("work", O_DIRECTORY);
+//	getdents(dfd0, buf, 65536);
+//	close_fd(dfd0);
+//	dfd0 = open("work", O_DIRECTORY);
+//	getdents(dfd0, buf, 65536);
+
+	asprintf(&filename4, "work/file_%d_%d.xml_checkxsltmerge.zip", threadnum, filenum);
+	if ((fd0 = open(filename4, O_CREAT|O_TRUNC|O_RDWR, 0644)) < 0) { // "work/file_1_1.xml_checkxsltmerge.zip"
+		printf("error opening %s: %m\n", filename4);
+		goto out;
+	}
+	if ((fd1 = open(filename3, O_RDONLY)) < 0) { // "work/file_1_1.xml"
+		printf("error opening %s: %m\n", filename3);
+		goto out;
+	}
+	read(fd1, buf, sizeof(buf));
+//	Data (1290 bytes)
+	write(fd0, buf, 816);
+//	Data (816 bytes)
+
+	asprintf(&filename5, "work/file_%d_%d.xml.tmp", threadnum, filenum);
+	if (stat(filename5, &st1) == 0 || errno != ENOENT) { // "work/file_1_1.xml.tmp"
+		if (errno != ENOENT)
+			printf("file '%s' should not have existed: %m\n", filename5);
+		else
+			printf("file '%s' should not have existed\n", filename5);
+		goto out;
+	}
+
+	if ((fd2 = open(filename5, O_CREAT|O_EXCL|O_RDWR, 0644)) < 0) { // "work/file_1_1.xml.tmp"
+		printf("error opening %s: %m\n", filename5);
+		goto out;
+	}
+	fsync(fd2);
+	ftruncate(fd2, 0);
+
+	if ((fd3 = open(filename5, O_CREAT|O_TRUNC|O_RDWR, 0644)) < 0) { // "work/file_1_1.xml.tmp"
+		printf("error opening %s: %m\n", filename5);
+		goto out;
+	}
+	write(fd2, buf, 1256);
+
+
+	close_fd(fd3);
+	close_fd(fd2);
+	close_fd(fd1);
+
+	stat(filename5, &st1);
+	if (rename(filename5, filename3) < 0) { // "work/file_1_1.xml.tmp", "work/file_1_1.xml"
+		printf("error renaming %s -> %s: %m\n", filename5, filename3);
+		goto out;
+	}
+	stat(filename3, &st2);
+
+	if ((intmax_t)st1.st_size != (intmax_t)st2.st_size) {
+		printf("file size prior to rename: %ld, file size after rename: %ld\n", (intmax_t)st1.st_size, (intmax_t)st2.st_size);
+		goto out;
+	}
+	if ((fd1 = open(filename3, O_RDONLY)) < 0) { //"work/file_1_1.xml"
+		printf("error opening %s: %m\n", filename3);
+		goto out;
+	}
+	read(fd1, buf, sizeof(buf)); // 1256
+
+	close_fd(fd0);
+	close_fd(fd1);
+
+	if (unlink(filename3) < 0) {// "work/file_1_1.xml"
+		printf("error removing %s: %m\n", filename3);
+		goto out;
+	}
+
+	ret = EXIT_SUCCESS;
+out:
+	close_fd(fd0);
+	close_fd(fd1);
+	close_fd(fd2);
+	close_fd(fd3);
+	free_str(filename0);
+	free_str(filename1);
+	free_str(filename2);
+	free_str(filename3);
+	free_str(filename4);
+	free_str(filename5);
+
+	exit(ret);
+}
+
+int main(int argc, char *argv[]) {
+	// create NUM_THREADS threads to simultaneously process their files
+	process_one(1, 1);
+}
